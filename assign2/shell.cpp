41c96d297516c5be7220dd52cd8e7aaa06a8fec7
jedsaro <jedsaro@gmail.com>
2022-02-26 14:10:07 -0800

    more testing

--- assign2/shell.cpp
+++ assign2/shell.cpp
@@ -3,9 +3,11 @@
 
 #define MAX_LINE 80 /* The maximum length command */
 
+//TODO: get user input
+
 int main(void)
 {	
-	char *args[ MAX_LINE/2 + 1]; /* command line arguments */
+	char *args[MAX_LINE/2 + 1]; /* command line arguments */
 
   	int should_run = 1; /* flag to determine when to exit program */
 
@@ -22,12 +24,12 @@ int main(void)
 		//execvp(, args)recive array 
 
 
-/**
-* After reading user input, the steps are:
-* (1) fork a child process using fork()
-* (2) the child process will invoke execvp()
-* (3) parent will invoke wait() unless command included &
-*/
+		/**
+		* After reading user input, the steps are:
+		* (1) fork a child process using fork()
+		* (2) the child process will invoke execvp()
+		* (3) parent will invoke wait() unless command included &
+		*/
 
 }
 	return 0;
