8728fea860e844541e89c075c433996ca794294b
jedsaro-ubuntu-home <jedsaro@gmail.com>
2022-03-04 23:59:12 -0800

    03/04/21

--- assign2/shell.cpp
+++ assign2/shell.cpp
@@ -18,44 +18,87 @@ using namespace std;
 
 // TODO: create a .h file and clean code into functions if possible
 
-int main(int argc, char **argv)
+void executeChild(char *array)
+{
+  // Forking a child
+  pid_t pid = fork();
+
+  if (pid == 0)
+  {
+    if (execvp(&array[0], &array) < 0)
+    {
+      printf("\nCould not execute command..");
+    }
+    exit(0);
+  }
+  else if (pid == -1)
+  {
+    printf("\nFork Failed");
+    return;
+  }
+  else
+  {
+    wait(NULL);
+    return;
+  }
+}
+
+
+bool input(char* array){
+
+  cin.getline(array, MAX_LINE);
+
+  if (sizeof(*array) != 1){
+
+    //!(@briana) add history here
+    //ex.. history(array);
+
+    return false;
+  }
+
+  cout << "NO COMMANDS" << endl;
+
+  return true;
+}
+
+
+int main(void)
 {
 
   // char *args[MAX_LINE / 2 + 1]; /* command line arguments */
 
+
   //* user input--------------------------
 
-  char userInput[MAX_LINE];
+  char userInput[MAX_LINE]; 
 
-  cin.getline(userInput, MAX_LINE);
+  //char *args[MAX_LINE/2 + 1] ;
 
-  //*parsing to argv-------------------------
 
-  argv[1] = &userInput[0];
+  int should_run = 1; /* flag to determine when to exit program */
 
-  /*  
-  args[2] = &userInput[1];
-  args[3] = NULL;
-  */
-  // pid_t pid = fork();
+  while (should_run)
+  {
 
-  /*
-    while (1)
-    {
+    printf("osh-> ");
+    fflush(stdout);
 
-      if (pid == 0)
-      {
-        execvp(args[0], args);
-      }
+    if(input(userInput)){
+      continue;
+    }
 
-     /*  if (pid > 0){
+    executeChild(userInput);
 
-        if (wait(NULL) == -1){
+    /**
+     * After reading user input, the steps are:
+     * (1) fork a child process using fork()
+     * (2) the child process will invoke execvp()
+     * (3) parent will invoke wait() unless command included &
+     */
 
-          perror(":C ");
-        }
-
-      } */
+    break;
+  }
 
   return 0;
+
 }
